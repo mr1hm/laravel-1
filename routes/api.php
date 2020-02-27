0c72f539721bbe71d52e905b2daef27a1c5cfb0e
Kevin <kevinihm@gmail.com>
2020-02-26 22:39:33 -0800

    removed route

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Http\Request;
 
 Route::get('/users', 'UsersApiController@index');
 
-// Route::get('/users/create', 'UsersApiController@create');
-
 Route::post('/users/create', 'UsersApiController@store');
 
 Route::get('/users/{userID}', 'UsersApiController@show');
@@ -25,8 +23,3 @@ Route::get('/users/{userID}', 'UsersApiController@show');
 Route::get('/users/{user}/edit', 'UsersApiController@edit');
 
 Route::post('/users/update/{user}', 'UsersApiController@update');
-
-// If you would like to use the method override way using {{ method_field('PATCH') }}:
-// 1. Comment out the the Route::post('/users/update/{user}', ...) and uncomment the line of code underneath.
-// 2. Send a PATCH request to http://laravel-1.test/api/users/update/{userID}
-// Route::patch('/users/update/{user}', 'UsersApiController@update');
