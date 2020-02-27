3433042f049152e2c54da9933f2c0e5a444a0538
Kevin <kevinihm@gmail.com>
2020-02-26 19:01:15 -0800

    removed comments

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,3 @@ Route::post('/users/create', 'UsersApiController@create');
 Route::get('/users/{userID}', 'UsersApiController@getUser');
 
 Route::post('/users/update/{user}', 'UsersApiController@updateUser');
-
-// If you would like to use the method override way using {{ method_field('PATCH') }}:
-// 1. Comment out the the Route::post('/users/update/{user}', ...) and uncomment the line of code underneath.
-// 2. Send a PATCH request to http://laravel-1.test/api/users/update/{userID}
-// Route::patch('/users/update/{user}', 'UsersApiController@update');
